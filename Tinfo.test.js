08cff6f49bc045365f357be847b7f4670c92a2d7
tungmaimcs <tungmaimcs@gmail.com>
2021-04-04 22:18:00 +0700

    ghj

---
+++ Tinfo.test.js
@@ -0,0 +1,22 @@
+var Tinfo = require('testtools').load('./Tinfo.js', 'Tinfo');
+
+describe('Tinfo', () => {
+
+    test('Propertive of Tinfo', () => {
+        expect(Tinfo).toHaveProperty('name');
+        expect(Tinfo).toHaveProperty('car');
+        expect(Tinfo).toHaveProperty('professional');
+    });
+
+    test('name', () => {
+        expect(Tinfo.name).toBe('Tung');
+    });
+
+    test('car', () => {
+        expect(Tinfo.car).toContain('Ford');    
+    });
+
+    test('professional', () => {
+        expect(Tinfo.pro).toMatch('design');
+    });
+});
